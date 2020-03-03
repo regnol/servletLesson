3d1d2d0e04ecb1d40fbea0fdef31a1947b779292
bruse <revreswzl@gmail.com>
2020-03-03 13:21:31 +0800

    新事物的提交

---
+++ CreateAccountServlet.java
@@ -0,0 +1,118 @@
+package org.wzl.yanyan.ch04.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.*;
+import java.sql.*;
+
+
+
+public class CreateAccountServlet extends HttpServlet{
+	
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 139349841127287424L;
+
+	private String name;
+	
+	private String password;
+	private String url;
+	
+	
+	public void init()throws ServletException {
+		ServletContext sc = getServletContext();
+		
+		String driverClass = sc.getInitParameter("driverClass");
+		
+		url = sc.getInitParameter("url");
+		name = sc.getInitParameter("user");
+		password = sc.getInitParameter("password");
+
+		System.out.println("url:" + url);
+		System.out.println("name:" + name);
+		System.out.println("password:" +password );
+		
+		try {
+			Class.forName(driverClass);	
+		} catch(ClassNotFoundException ex){
+			throw new ServletException("加载数据失败");
+		}
+	}
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException {
+		Connection conn = null;
+		PreparedStatement pre = null;
+
+		Statement stmt = null;
+		
+		try {
+			
+			conn = DriverManager.getConnection(url,name,password);
+			
+			stmt = conn.createStatement();
+			
+
+			String sql1 = "create table account (userid varchar(10) not null primary key,blance Float(6,2))";
+
+			String sql2 = "insert into account values(?,?)";
+
+			stmt.executeUpdate(sql1);
+
+			pre = conn.prepareStatement(sql2);
+
+			pre.setString(1,"甲");
+			pre.setFloat(2, 200.00f);
+
+			pre.setString(1,"乙");
+			pre.setFloat(2, 600.00f);
+
+			pre.executeUpdate();
+
+			res.setContentType("text/html;charset=utf-8");
+
+			PrintWriter out = res.getWriter();
+
+			out.println("创建表成功");
+
+			out.close();
+
+			
+		} catch (SQLException se){
+			se.printStackTrace();
+		} finally {
+
+			if(stmt != null){
+				try {
+					stmt.close();
+				} catch(SQLException se){
+					se.printStackTrace();
+				}
+				stmt = null;
+			}
+			
+			if(conn != null){
+				try{
+					conn.close();
+				} catch(SQLException se){
+					se.printStackTrace();
+				}
+				conn = null;
+			}
+
+			if(pre != null){
+				try{
+					pre.close();
+				} catch(SQLException se){
+					se.printStackTrace();
+				}
+				pre = null;
+			}
+		}
+	}
+
+	@Override
+	public void  doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException {
+		doGet(req,res);
+	}
+}
--- ListServlet.java
+++ ListServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,6 +13,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.*;
 
 public class ListServlet extends HttpServlet {
 	
@@ -24,15 +26,15 @@ public class ListServlet extends HttpServlet {
 		
 		ServletContext sc = getServletContext();
 		
-		String driverClass = sc.getParameter("driverClass"):
+		String driverClass = sc.getInitParameter("driverClass");
 		
 		try {
 			
 			Class.forName(driverClass);
 			
-			url = sc.getParameter("url");
-			name = sc.getParameter("name");
-			password = sc.getParameter("password");
+			url = sc.getInitParameter("url");
+			name = sc.getInitParameter("user");
+			password = sc.getInitParameter("password");
 		} catch(ClassNotFoundException e) {
 			throw new ServletException("加载数据库驱动失败");
 		}
@@ -42,30 +44,121 @@ public class ListServlet extends HttpServlet {
 	
 	
 	@Override
-	public void doGet(HttpServletRequest rq,HttpServletResponse rp) throws ServletException,IOException{
+	public void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException{
 		Connection conn = null;
 		Statement stmt = null;
 		
 		ResultSet rs = null;
 		
-		String condition  = req.getParameter("condition");
+		String condition  = req.getParameter("cond");
 		
 		if(null == condition || "".equals(condition)) {
-			resp.sendRedirect("search.html"):
+			resp.sendRedirect("search.html");
 			return;
 		}
 		
 		
 		
 		try {
+			
+			System.out.println("url:"+url);
+			System.out.println("name:"+name);
+			System.out.println("password:"+password);
+			
 			conn = DriverManager.getConnection(url,name,password);
 			stmt = conn.createStatement();
 			
+			resp.setContentType("text/html;charset=utf-8");
 			
-			stmt.
+			StringBuffer sql = new StringBuffer();
+			
+			switch(condition) {
+				
+				case "all": 
+							sql.append("select * from actor");
+							break;
+				case "precision": String first_name = req.getParameter("first_name");
+								  String last_name = req.getParameter("last_name");
+								  if(first_name == null || "".equals(first_name)){
+									  break;
+								  }
+								  if(last_name == null || "".equals(last_name)){
+									  break;
+								  }
+								  sql.append("select * from actor where last_name=");
+								  sql.append("'"+last_name+"'");
+								  sql.append("and first_name = ");
+								  sql.append("'"+first_name+"'");
+								  break;
+				case "key" : String keyworld = req.getParameter("keyworld");System.out.println("keyworld:"+ keyworld);
+							if(null == keyworld || "".equals(keyworld)){
+								break;
+							}
+							sql.append("select * from actor where first_name like ");
+							sql.append("'%");
+							sql.append(keyworld);
+							sql.append("%'");
+							break;
+							
+				default:break;
+			}
+			
+			String l = sql.toString();
+			
+			if(l == null || "".equals(l)){
+				resp.sendRedirect("search.html");
+				return;
+			}
+			
+			rs = stmt.executeQuery(l);
+			System.out.println(l);
+			PrintWriter out = resp.getWriter();
+			printActorInfo(out,rs);
+			out.close();
+			
 		} catch (SQLException se) {
-			// se.printStackTrace():
-			throw new ServletException(se):
+			throw new ServletException(se);
+		} finally {
+			if(stmt != null){
+				try {
+					stmt.close();
+				} catch(SQLException se){
+					se.printStackTrace();
+				}
+				stmt = null;
+			}
+			
+			if(conn != null){
+				try{
+					conn.close();
+				} catch(SQLException se){
+					se.printStackTrace();
+				}
+				conn = null;
+			}
+		}
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException{
+		doGet(req,resp);
+	}
+	
+	private void printActorInfo(PrintWriter out,ResultSet rs) throws SQLException {
+		
+		out.println("<html><head><title>演员信息表</title></head><body>");
+		out.println("<table border=1> <caption>演员表</caption>");
+		out.println("<tr><th>id</th><th>first_name</th><th>last_name</th><th>日期</th></tr>");
+		
+		while (rs.next()){
+			out.println("<tr>");
+			
+			out.println("<td>"+rs.getString("actor_id")+"</td>");
+			out.println("<td>"+rs.getString("first_name")+"</td>");
+			out.println("<td>"+rs.getString("last_name")+"</td>");
+			out.println("<td>"+rs.getString("last_update")+"</td>");
+			
+			out.println("</tr>");
 		}
 	}
 }
